9255e9eca1f91f08e2b5689d0d4a17046995a0b9
Cole <cole.d.comstock@gmail.com>
2020-09-29 14:20:37 -0600

    Function for finding total number of months - bank-analysis

--- bank-analysis/main-bank.py
+++ bank-analysis/main-bank.py
@@ -3,8 +3,15 @@ import csv
 
 bankcsv_path = os.path.join("Resources", "budget_data.csv")
 
+def fiscal_months(): 
+    total_months = [row for row in csvreader]
+    print(len(total_months))
+
+def months_profitable():
+    
+    
 with open(bankcsv_path, mode='r', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    for row in csvreader: 
-        print(row)
+    csv_header = next(csvreader)
+    
+    fiscal_months()
