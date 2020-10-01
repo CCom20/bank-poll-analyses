38e8fe039ffdd593f17d8d6e26c854890bbe0045
Cole <cole.d.comstock@gmail.com>
2020-10-01 14:39:33 -0600

    Finished with the csv writer output. Need to review project for all necessary aspects and add comments.

--- poll-analysis/main-poll.py
+++ poll-analysis/main-poll.py
@@ -2,8 +2,9 @@ import os
 import csv
 
 polldata_path = os.path.join("Resources", "election_data.csv")
-pollout_path = os.path.join("Output", "election_summary.csv")
+pollout_path = os.path.join("Output", "election_summary.txt")
 
+# Define the print function for Election Results
 def print_results():
     print("-------------------")
     print("Election Results")
@@ -19,7 +20,7 @@ def print_results():
     print(f"Winner: {winner}")
     print("-------------------")
 
-
+# Set Variables, Dictionaries, and Lists that we will be usings / appending to
 total_votes = 0
 winner_votes = 0
 candidate_votes = []
@@ -30,14 +31,19 @@ votes = {"Khan": [],
     "Tooley": []}
 winner = ""
 
+# Read CSV and start For-Loop to go through, read and append as appropriate
 with open(polldata_path, mode='r', newline='') as polldata:
     csvreader = csv.reader(polldata, delimiter=',')
     polldata_header = next(csvreader)
 
     for row in csvreader: 
         total_votes += 1
+
+        # Add name to names.list - Not necessary for this, but larger datasets will require a way to check all unique values, so I included this
         if row[2] not in names:
             names.append(row[2])
+
+        #Go through for-loop and append voter id to key-value in dictionary, then read the length of the list for an integer and calculate percentage of votes
         if row[2] == "Khan":
             votes["Khan"].append(row[0])
             percent_votes_khan = round(100 * (len(votes["Khan"]) / total_votes), 3)
@@ -51,6 +57,7 @@ with open(polldata_path, mode='r', newline='') as polldata:
             votes["Tooley"].append(row[0])
             percent_votes_tooley = round(100 * (len(votes["Tooley"]) / total_votes), 3)
 
+    #Check for winner by updating winner_votes and winner = ""
     if len(votes["Khan"]) > winner_votes:
         winner_votes = len(votes["Khan"])
         winner = "Khan"
@@ -66,4 +73,20 @@ with open(polldata_path, mode='r', newline='') as polldata:
 
     print_results()
 
-# with open(pollout_path, mode='w', newline='') as csvfile:
+# Export results as txt file
+with open(pollout_path, mode='w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    csvwriter.writerow(["-------------------"])
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["-------------------"])
+    csvwriter.writerow([f"Total Votes: {total_votes}"])
+    csvwriter.writerow([f"Candidates Running: {names[0]}, {names[1]}, {names[2]}, {names[3]}"])
+    csvwriter.writerow(["-------------------"])
+    csvwriter.writerow([f"{names[0]}: {percent_votes_khan}% (Total Votes: {len(votes['Khan'])})"])
+    csvwriter.writerow([f"{names[1]}: {percent_votes_correy}% (Total Votes: {len(votes['Correy'])})"])
+    csvwriter.writerow([f"{names[2]}: {percent_votes_li}% (Total Votes: {len(votes['Li'])})"])
+    csvwriter.writerow([f"{names[3]}: {percent_votes_tooley}% (Total Votes: {len(votes['Tooley'])})"])
+    csvwriter.writerow(["-------------------"])
+    csvwriter.writerow([f"Winner: {winner}"])
+    csvwriter.writerow(["-------------------"])
