e16cec9907057e1a5d060e365291748b0d900c9c
Cole <cole.d.comstock@gmail.com>
2020-10-01 12:01:26 -0600

    90% done with main assignments. However, I'd like to try to add a few more things to make the analyses a bit more robust.

--- poll-analysis/main-poll.py
+++ poll-analysis/main-poll.py
@@ -2,43 +2,52 @@ import os
 import csv
 
 polldata_path = os.path.join("Resources", "election_data.csv")
+pollout_path = os.path.join("Output", "election_summary.csv")
 
-total_votes = []
+def print_results():
+    print("-------------------")
+    print("Election Results")
+    print("-------------------")
+    print(f"Total Votes: {total_votes}")
+    print(f"Candidates Running: {names[0]}, {names[1]}, {names[2]}, {names[3]}")
+    print("-------------------")
+    print(f"{names[0]}: {percent_votes_khan}% (Total Votes: {len(votes['Khan'])})")
+    print(f"{names[1]}: {percent_votes_correy}% (Total Votes: {len(votes['Correy'])})")
+    print(f"{names[2]}: {percent_votes_li}% (Total Votes: {len(votes['Li'])})")
+    print(f"{names[3]}: {percent_votes_tooley}% (Total Votes: {len(votes['Tooley'])})")
+    print("-------------------")
+    print(f"Winner: {winner}")
+    print("-------------------")
+
+
+total_votes = 0
 candidate_votes = []
-unique_candidates = []
+names = []
 votes = {"Khan": [], 
     "Correy": [], 
     "Li": [], 
-    "O'Tooley": []}
-votes_khan = []
-votes_correy = []
-votes_li = []
-votes_otooley = []
+    "Tooley": []}
+winner = ""
 
 with open(polldata_path, mode='r', newline='') as polldata:
     csvreader = csv.reader(polldata, delimiter=',')
     polldata_header = next(csvreader)
 
-    percent_vote = 0
-
     for row in csvreader: 
-        total_votes.append(row[0])
-        if row[2] not in unique_candidates:
-            unique_candidates.append(row[2])
+        total_votes += 1
+        if row[2] not in names:
+            names.append(row[2])
         if row[2] == "Khan":
-            votes_khan.append(row[0])
-            percent_votes_khan = round(100 * (len(votes_khan) / len(total_votes)), 2)
+            votes["Khan"].append(row[0])
+            percent_votes_khan = round(100 * (len(votes["Khan"]) / total_votes), 3)
         elif row[2] == "Correy":
-            votes_correy.append(row[0])
+            votes["Correy"].append(row[0])
+            percent_votes_correy = round(100 * (len(votes["Correy"]) / total_votes), 3)
         elif row[2] == "Li":
-            votes_li.append(row[0])
+            votes["Li"].append(row[0])
+            percent_votes_li = round(100 * (len(votes["Li"]) / total_votes), 3)
         elif row[2] == "O'Tooley":
-            votes_otooley.append(row[0]) 
-        
-print(f"Candidates Running: {unique_candidates[0]}, {unique_candidates[1]}, {unique_candidates[2]}, {unique_candidates[3]}")
-print(f"Total Votes Cast: {len(total_votes)}")
-print(len(votes_khan))
-print(percent_votes_khan)
-print(len(votes_correy))
-print(len(votes_li))
-print(len(votes_otooley))
+            votes["Tooley"].append(row[0])
+            percent_votes_tooley = round(100 * (len(votes["Tooley"]) / total_votes), 3)
+
+    print_results()
